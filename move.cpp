293b86e5da8c740e01f1a01428efb050c85d85ac
Salman Cheema <salmancheema947@gmail.com>
2025-06-12 01:05:02 +0100

    Added logic for pawn moving forwards and left and right captures, excluding en passant and king checks

---
+++ move.cpp
@@ -0,0 +1,112 @@
+#include <vector>
+#include <cstdint>
+#include <cassert>
+#include <chess_types.h>
+#include "board.h"
+#include "move.h"
+
+using Bitboard = Chess::Bitboard;
+using Piece = Chess::PieceType;
+using Colour = Chess::PieceColour;
+
+std::vector<Move> MoveGenerator::legalMoves(const Board& board, Piece piece, 
+                                            Colour colour, uint8_t currSquare) {
+    switch (piece) {
+        case Piece::PAWN:
+            return legalPawnMoves(board, piece, colour, currSquare);
+            break;
+        case Piece::KNIGHT:
+            return legalKnightMoves(board, piece, colour, currSquare);
+            break;
+        case Piece::BISHOP:
+            return legalBishopMoves(board, piece, colour, currSquare);
+            break;
+        case Piece::ROOK:
+            return legalRookMoves(board, piece, colour, currSquare);
+            break;
+        case Piece::QUEEN:
+            return legalQueenMoves(board, piece, colour, currSquare);
+            break;
+        case Piece::KING:
+            return legalKingMoves(board, piece, colour, currSquare);
+            break;
+        default:
+            assert(false && "Piece must be either PAWN, KNIGHT, BISHOP, ROOK, QUEEN or KING");
+    }
+}
+
+std::vector<Move> MoveGenerator::legalPawnMoves(const Board& board, Piece piece, 
+                                                Colour colour, uint8_t currSquare) {
+    std::vector<Move> moves;
+    Bitboard piecesBitboard = board.getPiecesBitboard();
+    int8_t direction = (colour == Colour::WHITE) ? 1 : -1;
+    uint8_t nextSquare = currSquare += 8 * direction;
+
+    if ((piecesBitboard & (1ULL << nextSquare)) == 0) {
+        moves.push_back(makeMove(piece, colour, currSquare, nextSquare));
+
+        nextSquare += direction * 8;
+        if (nextSquare < 64) {
+            uint8_t firstUnmovedPawnSquare = 28 - 20 * direction;
+            bool isUnmoved = firstUnmovedPawnSquare <= currSquare && currSquare < firstUnmovedPawnSquare + 8;
+            if (isUnmoved && ((piecesBitboard & (1ULL << nextSquare)) == 0)) {
+                moves.push_back(makeMove(piece, colour, currSquare, nextSquare));
+            }
+        }
+    }
+
+    Bitboard opposingBitboard = (colour == Colour::WHITE) ?
+                                board.getBlackPiecesBitboard() :
+                                board.getWhitePiecesBitboard();
+    if ((currSquare & 0x7) != 0) {
+        uint8_t nextLeftSquare = currSquare + 8 * direction - 1;
+        if ((opposingBitboard & (1ULL << nextLeftSquare)) != 0) {
+            moves.push_back(makeMove(piece, colour, currSquare, nextLeftSquare, true));
+        }
+    }
+
+    if ((currSquare & 0x7) != 7) {
+        uint8_t nextRightSquare = currSquare + 8 * direction + 1;
+        if ((opposingBitboard & (1ULL << nextRightSquare)) != 0) {
+            moves.push_back(makeMove(piece, colour, currSquare, nextRightSquare, true));
+        }
+    }
+
+    return moves;
+}
+
+std::vector<Move> MoveGenerator::legalKnightMoves(const Board& board, Piece piece, 
+                                                  Colour colour, uint8_t currSquare) {
+
+}
+
+std::vector<Move> MoveGenerator::legalBishopMoves(const Board& board, Piece piece, 
+                                                  Colour colour, uint8_t currSquare) {
+
+}
+
+std::vector<Move> MoveGenerator::legalRookMoves(const Board& board, Piece piece, 
+                                                Colour colour, uint8_t currSquare) {
+
+}
+
+std::vector<Move> MoveGenerator::legalQueenMoves(const Board& board, Piece piece, 
+                                                 Colour colour, uint8_t currSquare) {
+
+}
+
+std::vector<Move> MoveGenerator::legalKingMoves(const Board& board, Piece piece, 
+                                                Colour colour, uint8_t currSquare) {
+
+}
+
+Move MoveGenerator::makeMove(Chess::PieceType piece, Chess::PieceColour colour, 
+                             uint8_t fromSquare, uint8_t toSquare, bool capture = false) {
+    return {
+        .piece = piece,
+        .colour = colour,
+        .fromSquare = fromSquare,
+        .toSquare = toSquare,
+        .capture = capture
+    };
+}
--- move.h
+++ move.h
@@ -1,13 +1,42 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-/**
- * Class which provides functions to move pieces from
- * one location to another
- */
-class Move {
-public:
+#include <vector>
+#include <cstdint>
+#include "chess_types.h"
+#include "board.h"
 
+/**
+ * Struct holding data to represent a move
+ */
+struct Move {
+    Chess::PieceType piece;
+    Chess::PieceColour colour;
+    uint8_t fromSquare;
+    uint8_t toSquare;
+    bool capture = false;
+};
+
+/**
+ * Class which provides provides functions to obtain
+ * legal moves
+ */
+class MoveGenerator {
+public:
+    using Piece = Chess::PieceType;
+    using Colour = Chess::PieceColour;
+    static std::vector<Move> legalMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+
+private:
+    static std::vector<Move> legalPawnMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+    static std::vector<Move> legalKnightMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+    static std::vector<Move> legalBishopMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+    static std::vector<Move> legalRookMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+    static std::vector<Move> legalQueenMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+    static std::vector<Move> legalKingMoves(const Board& board, Piece piece, Colour colour, uint8_t currSquare);
+
+    static inline Move makeMove(Chess::PieceType piece, Chess::PieceColour colour, 
+                                uint8_t fromSquare, uint8_t toSquare, bool capture);
 };
 
 #endif // MOVE_H
