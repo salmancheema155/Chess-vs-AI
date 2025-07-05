7ad235c60a9729ef9002675358bbd4b99fcb264d
Salman Cheema <salmancheema947@gmail.com>
2025-07-05 12:04:38 +0100

    Added function to make move

--- backend/include/game/game.h
+++ backend/include/game/game.h
@@ -16,6 +16,14 @@ public:
     Game();
 
     /**
+     * @brief Makes a move updating the game state
+     * @param fromSquare Square to move the piece from
+     * @param toSquare Square to move the piece to
+     * @return True if the move is legal, false otherwise
+     */
+    bool makeMove(uint8_t fromSquare, uint8_t toSquare);
+
+    /**
      * @brief Reverts the game state to the previous position before the most recent move
      * @return True if there is a move to undo, false otherwise (board is at the starting position)
      */
@@ -26,6 +34,15 @@ private:
     Board board;
     std::stack<GameState> gameStateHistory;
     std::stack<Move> moveHistory;
+
+    // /**
+    //  * @brief Checks if a move is legal
+    //  * @param piece Piece to move
+    //  * @param colour Colour of piece
+    //  * @param fromSquare Square to move the piece from
+    //  * @param toSquare Square to move the piece to
+    //  */
+    // bool isLegalMove(Piece piece, Colour colour, uint8_t fromSquare, uint8_t toSquare);
 };
 
 #endif // GAME_H
--- backend/src/game/game.cpp
+++ backend/src/game/game.cpp
@@ -1,16 +1,59 @@
 #include <stack>
+#include <vector>
+#include <optional>
+#include <cstdint>
 #include "game/game.h"
 #include "game/game_state.h"
 #include "board/board.h"
+#include "move/move_generator.h"
 #include "zobrist_hash.h"
 #include "chess_types.h"
 
+using Piece = Chess::PieceColour;
 using Colour = Chess::PieceColour;
+
+namespace {
+    std::optional<Move> searchLegalMoves(const std::vector<Move>& legalMoves, uint8_t fromSquare, uint8_t toSquare) {
+        for (const Move& move : legalMoves) {
+            if (move.getFromSquare() == fromSquare && move.getToSquare() == toSquare) {
+                return std::optional<Move>(move);
+            }
+        }
+        return std::nullopt;
+    }
+}
 
 Game::Game() {
     Colour currentTurn = Colour::WHITE;
     uint64_t hash = Zobrist::computeInitialHash(board, currentTurn);
     gameStateHistory.push(createGameState(currentTurn, board.getEnPassantSquare(), board.getCastlingRights(), 0, 1, hash));
+}
+
+bool Game::makeMove(uint8_t fromSquare, uint8_t toSquare) {
+    auto [piece, colour] = board.getPieceAndColour(fromSquare);
+    if (!piece.has_value() || !colour.has_value()) return false;
+
+    std::vector<Move> legalMoves = MoveGenerator::legalMoves(board, *piece, *colour, fromSquare);
+    std::optional<Move> move = searchLegalMoves(legalMoves, fromSquare, toSquare);
+
+    if (!move.has_value()) return false;
+
+    board.makeMove(*move, *colour);
+    Colour newPlayerTurn = (*colour == Colour::WHITE) ? Colour::BLACK : Colour::WHITE;
+    uint16_t newFullMoves = (*colour == Colour::BLACK) ? currentState.fullMoves + 1 : currentState.fullMoves;
+    uint16_t newHalfMoves = ((*move).getCapturedPiece() == Move::NO_CAPTURE && *piece != Piece::PAWN) ?
+                            currentState.halfMoveClock + 1 :
+                            0;
+
+    auto newEnPassantSquare = board.getEnPassantSquare();
+    auto newCastlingRights = board.getCastlingRights();
+    uint64_t newHash = Zobrist::updateHash(currentState.hash, *move, currentState.enPassantSquare, newEnPassantSquare,
+                                            currentState.castleRights, newCastlingRights, *colour, *piece);
+
+    currentState = createGameState(newPlayerTurn, newEnPassantSquare, newCastlingRights, newHalfMoves, newFullMoves, newHash);
+    moveHistory.push(*move);
+    gameStateHistory.push(currentState);
+    return true;
 }
 
 bool Game::undo() {
